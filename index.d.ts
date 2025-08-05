4626173dc1667ea786a7a1d9333ee574ca647b4c
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-08-04 22:26:23 -0400

    adding PATH_PREFIX environment variable

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
-type EnvTypes = { STILL_HOME };
+type EnvTypes = { STILL_HOME, PATH_PREFIX? };
+type ContainerTypes = 'next'|'react'|'default';
 
 export interface LoneAppParams {
 
     env: EnvTypes;
-    container?: string | null;
+    container?: ContainerTypes;
   
 }
   
@@ -11,11 +12,19 @@ export class StillLoneApp {
     load(): void; unload(): void;
 }
 
+export interface ComponentReferencing {
+    /** @param { String } refName */
+    ref(refName): Object;
+    /**  @param { Array<String> } ref */
+    setRefs(refsList): StillAppLoader;
+}
+
 
 export class StillAppLoader {
   
     #script : HTMLScriptElement | any;
     #params: LoneAppParams;
+    component: ComponentReferencing;
 
     cdn(params: LoneAppParams): StillAppLoader
 
